352516b9f7dcdc734eca6ee0181912acda5a6c76
TJFRexeter <tjfr201@exeter.ac.uk>
2025-12-15 23:02:45 +0000

    formatting games (unfinished)

--- scraper.py
+++ scraper.py
@@ -1,14 +1,37 @@
 import os
 import re
 
+
+def scrape_positions(search_str : str, string : str):
+    positions:list[tuple[int,int]] = []
+    for match in re.finditer(search_str,string):
+        start = match.end()
+        stop = string.find("<",start)
+        positions.append((start,stop))
+    return positions
+
+def index_to_string(index:tuple[int,int],string:str) -> str:
+    i,j = index
+    word = string[i:j]
+    return word
+
+def str_fraction_to_float(fraction : str) -> float:
+    frac: list[str] = fraction.split("/",1)
+    numer,denom = frac
+    num = float(numer) / float(denom)
+    return num
+
+def show_pos_as_strings(string:str,positions:list[tuple[int,int]]) -> list[str]:
+    return [index_to_string(i,string) for i in positions]
+
 path = str(os.getcwd()) + "\\web_scraper\\"
 
-filename = "bet365.html"
+filename = "top.html"
 
 odds_search = '<span class="sgl-ParticipantOddsOnly80_Odds">'
-team_search = 'class="rcl-ParticipantFixtureDetailsTeam_TeamName ">'
-date_search = 'class="rcl-MarketHeaderLabel rcl-MarketHeaderLabel-leftalign ">'
-date_search2 = '"rcl-MarketHeaderLabel-leftalign rcl-MarketHeaderLabel ">'
+teams_search = 'class="rcl-ParticipantFixtureDetailsTeam_TeamName ">'
+teams_search2 = 'class="rcl-ParticipantFixtureDetailsTeam_TeamName " style="">'
+#dividers_search = '"suf-CompetitionMarketGroupButton_Text ">'
 
 with open(path+filename, 'rb') as f:
     try:  # catch OSError in case of a one line file
@@ -19,16 +42,19 @@ with open(path+filename, 'rb') as f:
         f.seek(0)
     last_line = f.readline().decode()
 
-def scrape(search_str : str, last_line : str):
-    vals:list[str] = []
-    for match in re.finditer(search_str,last_line):
-        start = match.end()
-        stop = last_line.find("<",start)
-        vals.append(last_line[start:stop])
-    return vals
+odds_positions = scrape_positions(odds_search,last_line)
+teams_positions = scrape_positions(teams_search,last_line) + scrape_positions(teams_search2,last_line)
+#dividers_positions = scrape_positions(dividers_search,last_line)
 
-odds = scrape(odds_search,last_line)
-teams = scrape(team_search,last_line)
-dates = scrape(date_search,last_line)+scrape(date_search2,last_line)
+all_positions = sorted(odds_positions+teams_positions)
 
-print(odds,teams,dates)
+
+
+matches:list[tuple[str,str]] = []
+for i in range(0,len(teams_positions),2):
+    team1 = index_to_string(teams_positions[i],last_line)
+    team2 = index_to_string(teams_positions[i+1],last_line)
+    match:tuple[str,str] = (team1,team2)
+    matches.append(match)
+
+print(show_pos_as_strings(last_line,sorted(odds_positions)))
